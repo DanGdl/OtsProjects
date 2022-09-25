2d2ff2cd2f6b37f14eb9c12d57ac7b1b0bdadc83
DanGdl <dangladste@gmail.com>
2022-09-25 10:30:45 +0300

    telehack

--- Hw12/telehack_client.c
+++ Hw12/telehack_client.c
@@ -1,79 +1,166 @@
+#define _GNU_SOURCE     /* To get defns of NI_MAXSERV and NI_MAXHOST */
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <linux/if_link.h>
+#include <netinet/in.h>
+#include <ifaddrs.h>
+#include <netpacket/packet.h>
+#include <errno.h>
+
 #include <stdio.h>
-#include <sys/stat.h>   // check file info
-#include <sys/mman.h>   // mmap
-#include <fcntl.h>      // permission flags
 #include <unistd.h>     // open/close
 #include <string.h>     // memcopy
 #include <stdint.h>
 
+#define URL "telehack.com"
+#define PORT "47"
 
-// #define SIGNATURE_START_OF_CENTRAL_RECORD 0x02014b50
-#define SIGNATURE_END_OF_CENTRAL_RECORD 0x06054b50
-#define SIGNATURE_START_OF_RECORD 0x04034b50
+#define CMD_TELEHACK "figlet /%s %s"
 
-#define OFFSET_CENTRAL_ENTRY_SIZE 12    // offset from SIGNATURE_END_OF_CENTERAL_RECORD
-#define OFFSET_ENTRY_NAME_SIZE 26       // offset from SIGNATURE_START_OF_RECORD
-#define OFFSET_ENTRY_NAME 30            // offset from SIGNATURE_START_OF_RECORD
+const char* const supported_fonts[] = { "3-d", "3x5", "5lineoblique", "acrobatic",
+		"alligator", "alligator2", "alphabet", "vatar", "banner", "banner3",
+		"banner3-D", "anner4", "barbwire", "basic", "bell", "ig", "bigchief",
+		"binary", "block", "ubble", "bulbhead", "calgphy2", "caligraphy",
+		"atwalk", "chunky", "coinstak", "colossal", "omputer", "contessa",
+		"contrast", "cosmic", "osmike", "cricket", "cursive", "cyberlarge",
+		"ybermedium", "cybersmall", "diamond", "digital", "oh", "doom",
+		"dotmatrix", "drpepper", "ftichess", "eftifont", "eftipiti",
+		"eftirobot", "ftitalic", "eftiwall", "eftiwater", "epic", "ender",
+		"fourtops", "fuzzy", "goofy", "othic", "graffiti", "hollywood",
+		"invita", "sometric1", "isometric2", "isometric3", "isometric4",
+		"talic", "ivrit", "jazmine", "jerusalem", "atakana", "kban", "larry3d",
+		"lcd", "ean", "letters", "linux", "lockergnome", "adrid", "marquee",
+		"maxfour", "mike", "ini", "mirror", "mnemonic", "morse", "oscow",
+		"nancyj", "nancyj-fancy", "nancyj-underlined", "ipples", "ntgreek",
+		"o8", "ogre", "awp", "peaks", "pebbles", "pepper", "oison", "puffy",
+		"pyramid", "rectangles", "elief", "relief2", "rev", "roman", "ot13",
+		"rounded", "rowancap", "rozzo", "unic", "runyc", "sblood", "script",
+		"erifcap", "shadow", "short", "slant", "lide", "slscript", "small",
+		"smisome1", "mkeyboard", "smscript", "smshadow", "smslant", "mtengwar",
+		"speed", "stampatello", "standard", "tarwars", "stellar", "stop",
+		"straight", "anja", "tengwar", "term", "thick", "hin", "threepoint",
+		"ticks", "ticksslant", "inker-toy", "tombstone", "trek", "tsalagi",
+		"wopoint", "univers", "usaflag", "wavy", "eird"
+};
 
 
-int main(int argc, char* argv[]) {
-    char* path = NULL;
-    if (argc == 2) {
-        path = argv[1];
-    } else {
-        printf("Please add a path to file to parameters\n");
-        return 0;
-    }
+void log_error(const char *message) {
+	fprintf(stderr, "%s %d: %s\n", message, errno, strerror(errno));
+}
 
-    struct stat stats;
-    if (stat(path, &stats) != 0) {
-        printf("Can't get state for path %s\n", path);
-    } else if (S_ISDIR(stats.st_mode)) {
-        printf("File is directory, path %s\n", path);
-    } else if (!S_ISREG(stats.st_mode)) {
-        printf("File is not regular file, path %s\n", path);
-    } else {
-        int fd = open(path, O_RDONLY);
-        if (fd < 0) {
-            printf("Failed to open file %s\n", path);
-            return 0;
-        }
-        uint8_t* mapped = mmap(NULL, stats.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-        if (mapped == MAP_FAILED) {
-            printf("Failed to map file to memory\n");
-            close(fd);
-            return 0;
-        }
-        uint16_t current_entry = 0;
-        for (long int i = stats.st_size - 1; i >= 0; i--) {
-            uint32_t signature = 0;
-            memcpy(&signature, mapped + i, sizeof(signature));
-            if (signature == SIGNATURE_END_OF_CENTRAL_RECORD) {
-                uint32_t central_dir_size = 0;
-                memcpy(&central_dir_size, mapped + i + OFFSET_CENTRAL_ENTRY_SIZE, sizeof(central_dir_size));
+void close_socket(int socket) {
+	if (socket > 0) {
+		close(socket);
+	}
+}
 
-                // jump to beginning of central entry
-                i = i - central_dir_size + 1;
-            } else if (signature == SIGNATURE_START_OF_RECORD) {
-                uint16_t name_size = 0;
-                memcpy(&name_size, mapped + i + OFFSET_ENTRY_NAME_SIZE, sizeof(name_size));
+int open_client_socket(const char *address, const char *port) {
+	struct addrinfo hints;
+	struct addrinfo *res = NULL;
 
-                char name[name_size + 1];
-                memcpy(&name, mapped + i + OFFSET_ENTRY_NAME, sizeof(name));
-                name[name_size] = '\0';
-                printf("Record %04d name %s\n", current_entry, (char*) &name);
-                current_entry++;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_protocol = 0;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_ALL;
+
+	int code = getaddrinfo(address, port, &hints, &res);
+	if (code != 0) {
+		fprintf(stderr, "Can't resolve address: %s\n", gai_strerror(code));
+		return -1;
+	}
+	const int socket_descriptor = socket(res->ai_family, res->ai_socktype,
+			res->ai_protocol);
+	if (socket_descriptor == -1) {
+		log_error("Create socket failed");
+		return -1;
+	}
+	code = connect(socket_descriptor, res->ai_addr, res->ai_addrlen);
+	if (code == -1) {
+		freeaddrinfo(res);
+		close_socket(socket_descriptor);
+		fprintf(stderr, "%d: %s\n", code, gai_strerror(code));
+		log_error("Can't connect");
+		return -1;
+	}
+	freeaddrinfo(res);
+	return socket_descriptor;
+}
+
+int network_send(const int socket, const uint8_t* const message, const int size) {
+	return send(socket, message, size, 0);
+}
+
+int network_receive(const int socket, uint8_t *buf, const int size) {
+	return recv(socket, buf, size, 0);
+}
+
+int main(int argc, char *argv[]) {
+	char *font = NULL;
+	char *text = NULL;
+	if (argc == 3) {
+		font = argv[1];
+		text = argv[2];
+	} else {
+		printf("Please provide font and text as parameters: ./telehack_client font text\n");
+		return 0;
+	}
+	int found = 0;
+	for (uint32_t i = 0; i < sizeof(supported_fonts) / sizeof(supported_fonts[0]); i++) {
+		if (strcmp(font, supported_fonts[i]) == 0) {
+			found = 1;
+			break;
+		}
+	}
+	if (!found) {
+		printf("Unsupported font %s. Must be onr of the list:\n", font);
+		for (uint32_t i = 0; i < sizeof(supported_fonts) / sizeof(supported_fonts[0]); i++) {
+			if (i % 4 == 0) {
+				printf("%s\n", supported_fonts[i]);
+			} else {
+				printf("%s\t", supported_fonts[i]);
+			}
+		}
+		return 0;
+	}
+
+	int socket = open_client_socket(URL, NULL);
+	if (socket > 0) {
+        while(1) {
+            int command_size = strlen(CMD_TELEHACK) + strlen(font) + strlen(text) + 1 - 4;
+            uint8_t* command = NULL;
+            command = calloc(sizeof(*command), command_size);
+            if (command == NULL) {
+                printf("Failed to allocate memory for command\n");
+                break;
             }
+            if (sprintf((char*) command, CMD_TELEHACK, font, text) <= 0) {
+                printf("Failed to setup command\n");
+                break;
+            }
+            if (network_send(socket, command, command_size) == -1) {
+                log_error("Failed to send command");
+                break;
+            }
+            uint8_t buffer[256] = { '\0' };
+            int total_received = 0;
+            int received = 0;
+            do {
+                received = network_receive(socket, buffer, sizeof(buffer) / sizeof(buffer[0]) - 1);
+                printf("Received: %d\n", received);
+                if (received > 0) {
+                    total_received += received;
+                    buffer[received] = '\0';
+                    printf("%s", buffer);
+                }
+            } while (received);
+            break;
         }
-
-        if (munmap(mapped, stats.st_size) != 0) {
-            printf("Could not munmap\n");
-        }
-        close(fd);
-
-        if (current_entry == 0) {
-            printf("File doesn't contains zip archive\n");
-        }
-    }
-    return 0;
+	}
+	close_socket(socket);
+	return 0;
 }
